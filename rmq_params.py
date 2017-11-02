61f349a9444ec5f76b33e60f347fad71316ab3ce
eric <ericjw@vt.edu>
2017-11-02 21:26:06 +0000

    added other stuff

---
+++ rmq_params.py
@@ -0,0 +1,9 @@
+rmq_params = {
+    "vhost": "<RMQ_VHOST_NAME>",
+    "username": "RMQ_USERNAME",
+    "password": "RMQ_PASSWORD",
+    "exchange": "RMQ_EXCHANGE",
+    "queues":{"<Q_1>", "<Q_2>", "<Q_3>", … , “<Q_i>”},
+    "master_queue":"<MASTER_QUEUE>",
+    "status_queue":"<STATUS_QUEUE>"
+}
