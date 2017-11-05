b298f148376ffe6e51682603b869a76ce8b7bd1a
eric <ericjw@vt.edu>
2017-11-05 19:42:45 +0000

    repo done?

--- repository.py
+++ repository.py
@@ -3,21 +3,37 @@ import sys
 from rmq_params import *
 
 credentials = pika.PlainCredentials(rmq_params['username'], rmq_params['password'])
-parameters = pika.ConnectionParameters(host=sys.argv[2], virtual_host=rmq_params['vhost'], credentials=credentials)
+parameters = pika.ConnectionParameters(host='localhost', virtual_host=rmq_params['vhost'], credentials=credentials)
 connection = pika.BlockingConnection(parameters)
-
 channel = connection.channel()
-channel.exchange_declare(exchange=rmq_params['exchange'], exchange_type='direct')
 
-channel.queue_declare(queue='fuck')
-print("adsf")
+print("[Checkpoint 01] Connected to vhost '{}' on RMQ server at 'localhost' as user '{}'".format(rmq_params['vhost'], rmq_params['username']))
+
+channel.exchange_declare(exchange=rmq_params['exchange'], exchange_type='direct')
+channel.queue_declare(queue=rmq_params['master_queue'])
+channel.queue_declare(queue=rmq_params['status_queue'])
+channel.queue_purge(queue=rmq_params['master_queue'])
+channel.queue_purge(queue=rmq_params['status_queue'])
+channel.queue_bind(exchange=rmq_params['exchange'], 
+                    queue=rmq_params['status_queue'], 
+                    routing_key=rmq_params['status_queue'])
+
 
 for item in list(rmq_params['queues']):
     channel.queue_declare(queue=item)
-    print('sdf')
-channel.queue_declare(queue=rmq_params['master_queue'])
-channel.queue_declare(queue=rmq_params['status_queue'])
+    channel.queue_purge(queue=item)
+    channel.queue_bind(exchange=rmq_params['exchange'], queue=item, routing_key=item)
+    channel.queue_bind(exchange=rmq_params['exchange'], queue=rmq_params['master_queue'], routing_key=item)
 
-print('here')
+def master_callback(ch, method, properties, body):
+    print("[Checkpoint 03] Consumed a message published with routing_key: '{}'".format(method.routing_key))
+    print("[Checkpoint 04] Message: {}".format(body))
     
+def status_callback(ch, method, properties, body):
+    print("[Checkpoint l-01] Flashing LED to {}".format(body))
+ 
+channel.basic_consume(master_callback, queue=rmq_params['master_queue'], no_ack=True)
+channel.basic_consume(status_callback, queue=rmq_params['status_queue'], no_ack=True)
+
+channel.start_consuming()
 
